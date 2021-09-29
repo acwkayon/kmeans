8b49898b983db64983d4b887f5b2e68cc3ff43d6
Gefei Shan <u6839726@anu.edu.au>
2021-09-29 12:09:03 +0800

    debug for demo

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@
 print("import requiring libraries")
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
-from utils import update, KMeans, view, prepare, join_path
+from utils import update, KMeans, view, prepare, join_path, save_animation, join_path
 import os
 
 if __name__ == '__main__':
@@ -14,5 +14,6 @@ if __name__ == '__main__':
     ani = animation.FuncAnimation(fig, update, frames=kmeans.itergenetor(),fargs=(kmeans,),interval=200)
     writer = animation.FFMpegWriter(
         fps=30, metadata=dict(artist='Me'), bitrate=1800)
-    ani.save("examples/movie.mp4")
+    #ani.save("examples/movie.mp4")
+    save_animation(ani, join_path("examples/movie.mp4"))
     view(os.path.abspath("examples/movie.mp4"))
