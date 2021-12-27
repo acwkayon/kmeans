394ad3b6fb6672c61c8de708e34c090a518331ad
Anita Williams <acwkayon@gmail.com>
2021-12-27 14:07:02 -0500

    revert previous change and fix style issues

--- predict.py
+++ predict.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# MLHub demonstrator and toolkit for kmeans.
+#
+# Time-stamp: <Monday 2021-12-27 14:06:00 PST Anita Williams>
+#
+# Authors: Gefei Shan, Graham.Williams@togaware.com, Anita@togaware.com
+# License: General Public License v3 GPLv3
+# License: https://www.gnu.org/licenses/gpl-3.0.en.html
+# Copyright: (c) Gefei Shan, Graham Williams. All rights reserved.
+
 import os
 import sys
 import click
@@ -7,23 +18,28 @@ import numpy as np
 
 from mlhub.pkg import get_cmd_cwd
 
+# Command line argument and options.
+
 
 @click.command()
-@click.argument('modelfile', 
-                type=click.File('r'), 
-                default=sys.stdin)
-# removed help="the model file for predictions, in csv format",
-@click.argument('csvfile', 
-                type=click.File('r'), 
-                default=sys.stdin)
-# removed  help="the input data file, in csv format",
-@click.option('-o', '--output', 
-            default=sys.stdout,type=click.File('w'), 
-            help="Save the output predictions to file.")
-def cli(modelfile, csvfile, output):
-   
-    os.chdir(os.getcwd())
+@click.argument('modelfile',
+                type=click.File('r'),
+                default=sys.stdin)  
+# help="model file for predictions, in csv format"
+@click.argument('csvfile',
+                type=click.File('r'),
+                default=sys.stdin) 
+# help="the input data file, in csv format"
+@click.option('-o', '--output',
+              default=sys.stdout,type=click.File('w'), 
+              help="Save the output predictions to file.")
 
+def cli(modelfile, csvfile, output):
+
+    # Ensure we look for the files in the user's cwd
+    os.chdir(get_cmd_cwd())
+
+    # Quit the program if there is not any data to read
     try:
         df = pd.read_csv(csvfile)
     except pd.errors.EmptyDataError:
@@ -31,15 +47,12 @@ def cli(modelfile, csvfile, output):
         sys.exit(1)
     data = df.to_numpy()
 
-
-    df_centers = pd.read_csv(modelfile, header=1) # modelfile, the centers of clusters
-    #df_data = pd.read_csv(csvfile) # might not need to read twice
-    
+    # modelfile, the centers of clusters
+    df_centers = pd.read_csv(modelfile, header=1)
     if "labels" in df_centers.columns:
         df_centers["label"] = df_centers["labels"]
         df_centers = df_centers.drop(columns="labels")
 
-    
     df_centers.sort_values(by="label", inplace=True)
     centers = df_centers.drop(columns="label").to_numpy()
     label_index = df_centers["label"]
@@ -48,24 +61,21 @@ def cli(modelfile, csvfile, output):
     data = df.to_numpy()
     n = data.shape[0]
     distance = np.zeros([n, k])
-    
     # calculate distance and assign labels
-    for j, c in enumerate(centers):
-        
+    for j, c in enumerate(centers): 
         distance[:, j] = np.linalg.norm(data - c, axis=1)
     new_labels = np.argmin(distance, axis=1)
-    df_labels = pd.Series(data=new_labels).map(label_index.to_dict()) # map labels from 0,...,k-1 to the provided label string
+    # map labels from 0,...,k-1 to the provided label string
+    df_labels = pd.Series(data=new_labels).map(label_index.to_dict())
 
     df["label"] = df_labels
 
     origin_out = sys.stdout
-    # redirect the output
+    #  redirect the output
     sys.stdout = output
 
-    print(df.to_csv(index = False).strip())
+    print(df.to_csv(index=False).strip())
 
 
-
-# copying from train.py, not sure about this yet:
 if __name__ == "__main__":
     cli(prog_name="predict")
