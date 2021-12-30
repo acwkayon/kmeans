690cd560d66ac6824bb384f5fd82497364973d35
Graham Williams <Graham.Williams@togaware.com>
2021-12-31 09:32:02 +1100

    Lint review.  Correct args.  Remove redundant.

--- predict.py
+++ predict.py
@@ -1,20 +1,18 @@
-# -*- coding: utf-8 -*-
-#
 # MLHub demonstrator and toolkit for kmeans.
 #
-# Time-stamp: <Wed 2021-12-29 14:59:54 -0500 Anita Williams>
+# Time-stamp: <Friday 2021-12-31 09:28:36 +1100 Graham Williams>
 #
-# Authors: Gefei Shan, Graham.Williams@togaware.com, Anita@togaware.com
+# Authors: Gefei Shan, Anita@togaware.com
 # License: General Public License v3 GPLv3
 # License: https://www.gnu.org/licenses/gpl-3.0.en.html
-# Copyright: (c) Gefei Shan, Graham Williams. All rights reserved.
+# Copyright: (c) Gefei Shan, Anita Williams. All rights reserved.
 
 import os
 import sys
 import click
 
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 from mlhub.pkg import get_cmd_cwd
 
@@ -22,58 +20,54 @@ from mlhub.pkg import get_cmd_cwd
 
 os.chdir(get_cmd_cwd())
 
-# Command line argument and options.
+
+# Command line interface, arguments and options.
+#
+# CSVFILE is required and is the data to be predicted from.
+# MODELFILE is required but can be obtained from stdin.
 
 
 @click.command()
 @click.argument('csvfile',
-                type=click.File('r'),
-                default=sys.stdin) 
-# help="the input data file, in csv format"
+                type=click.File('r'))
 @click.argument('modelfile',
                 type=click.File('r'),
-                default=sys.stdin)  
-# help="model file for predictions, in csv format"
+                default=sys.stdin)
+def cli(modelfile, csvfile):
 
-def cli(modelfile, csvfile, output):
+    # The datafile for predicting from is required.
 
-    # Ensure we look for the files in the user's cwd
-    os.chdir(get_cmd_cwd())
+    df = pd.read_csv(csvfile)
+    data = df.to_numpy()
 
-    # Quit the program if there is not any data to read
+    # The modelfile is required and records the centers of clusters. The
+    # label field reports the cluster number.
+
     try:
-        df = pd.read_csv(csvfile)
+        centers = pd.read_csv(modelfile)
     except pd.errors.EmptyDataError:
-        click.echo("Quitting as CSV file has no data available.")
+        click.echo("A model is required: consists of centers,label.")
         sys.exit(1)
-    data = df.to_numpy()
 
-    # modelfile, the centers of clusters
-    df_centers = pd.read_csv(modelfile)
-    if "labels" in df_centers.columns:
-        df_centers["label"] = df_centers["labels"]
-        df_centers = df_centers.drop(columns="labels")
+    # Prepare data.
 
-    df_centers.sort_values(by="label", inplace=True)
-    centers = df_centers.drop(columns="label").to_numpy()
-    label_index = df_centers["label"]
+    centers.sort_values(by="label", inplace=True)
+    labels = centers["label"]
+    centers = centers.drop(columns="label").to_numpy()
 
-    k, m = centers.shape
-    data = df.to_numpy()
-    n = data.shape[0]
-    distance = np.zeros([n, k])
-    # calculate distance and assign labels
-    for j, c in enumerate(centers): 
+    k, m = centers.shape  # k clusters and m variables.
+    n = data.shape[0]  # n observations to be classified.
+    distance = np.zeros([n, k])  # Initialise a distance matrix.
+
+    # Calculate distance and assign labels.
+
+    for j, c in enumerate(centers):
         distance[:, j] = np.linalg.norm(data - c, axis=1)
     new_labels = np.argmin(distance, axis=1)
-    # map labels from 0,...,k-1 to the provided label string
-    df_labels = pd.Series(data=new_labels).map(label_index.to_dict())
 
-    df["label"] = df_labels
+    # Map labels from 0,...,k-1 to the provided label string
 
-    origin_out = sys.stdout
-    #  redirect the output
-    sys.stdout = output
+    df["label"] = pd.Series(data=new_labels).map(labels.to_dict())
 
     print(df.to_csv(index=False).strip())
 
