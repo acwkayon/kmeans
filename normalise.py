7b2687efa134fc5f4438ae7231d789f91474a7c5
Graham Williams <Graham.Williams@togaware.com>
2021-12-30 16:46:23 +1100

    Lint/style updates.  Output stdout, not file.

--- normalise.py
+++ normalise.py
@@ -1,14 +1,11 @@
-# -*- coding: utf-8 -*-
-#
 # MLHub demonstrator and toolkit for kmeans.
 #
-# Time-stamp: <Wed 2021-12-29 16:22:13 -0500 Anita Williams>
+# Time-stamp: <Thursday 2021-12-30 16:44:08 +1100 Graham Williams>
 #
-# Authors: Gefei Shan, Graham.Williams@togaware.com, Anita@togaware.com
+# Authors: Anita@togaware.com
 # License: General Public License v3 GPLv3
 # License: https://www.gnu.org/licenses/gpl-3.0.en.html
-# Copyright: (c) Gefei Shan, Graham Williams. All rights reserved.
-
+# Copyright: (c) Anita Williams. All rights reserved.
 
 import os
 import sys
@@ -18,7 +15,6 @@ import pandas as pd
 import numpy as np
 
 from mlhub.pkg import get_cmd_cwd
-
 
 # Ensure paths are relative to the user's cwd.
 
@@ -30,22 +26,24 @@ os.chdir(get_cmd_cwd())
                 type=click.File('r'),
                 default=sys.stdin)
 # help="the input data file, in csv format"
-
 def cli(csvfile):
-    # Quit the program if there is not any data to read
+
+    # Quit if no data to read.
+
     try:
         df = pd.read_csv(csvfile)
     except pd.errors.EmptyDataError:
-        click.echo("Quitting as CSV file has no data available.")
+        click.echo("No data available.")
         sys.exit(1)
 
     df_std = df.copy()
- 
-    # apply the z-score method
+
+    # Apply the z-score method.
+
     for column in df_std.columns:
         df_std[column] = (df_std[column] - df_std[column].mean()) / df_std[column].std()
-      
-    df_std.to_csv('normalised.csv') 
+    
+    click.echo(df_std.to_csv(index=False))
 
 
 if __name__ == "__main__":
