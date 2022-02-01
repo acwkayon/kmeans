978e33628e0b1960ed87bcce2915e7827ca0b3c5
Anita Williams <acwkayon@gmail.com>
2022-01-31 20:34:13 -0800

    create a function to visualise based on number of columns, add some temporary print statements to demo

--- visualise.py
+++ visualise.py
@@ -3,26 +3,43 @@ import pandas as pd
 from plotnine import ggplot, aes, geom_point
 from sklearn.decomposition import PCA
 
-# labelled.csv is the output from predict.py using iris.csv
-df = pd.read_csv('labelled.csv')
+def visualise(df):
+      if len(df.columns) == 2:
+            header_1_col = list(df.columns.values)
+            return (ggplot(df) +
+            aes(x=header_1_col[0], y="label", color="factor(label)") +
+            geom_point())
+      if len(df.columns) == 3:
+            header_2_col = list(df.columns.values)
+            return (ggplot(df) +
+            aes(x=header_2_col[0], y=header_2_col[1], color="factor(label)") +
+            geom_point())
+      else:
+            pca = PCA(n_components=2)
+            principal_components = pca.fit_transform(df)
+            principal_df = pd.DataFrame(data=principal_components
+                            , columns=['PC 1', 'PC 2']
+                            )
+            principal_label_df = pd.concat([principal_df, df[['label']]], axis=1)
 
-# Use a scatter plot for 2 dimensions
-print(ggplot(df) +
-      aes(x="sepal_length", y="sepal_width", color="factor(label)") +
-      geom_point())
+            return (ggplot(principal_label_df) +
+            aes(x="PC 1", y="PC 2", color="factor(label)") +
+            geom_point())
 
-# Line of dots for 1 dimension
-print(ggplot(df) +
-      aes(x="sepal_length", y="label", color="factor(label)") +
-      geom_point())
+# examples for demo-ing
 
-# Do PCA to find the two most significant components for > 2 dimensions
-pca = PCA(n_components=2)
-principal_components = pca.fit_transform(df)
-principal_df = pd.DataFrame(data=principal_components,
-                            columns=['PC 1', 'PC 2'])
-principal_label_df = pd.concat([principal_df, df[['label']]], axis=1)
+# output of preditc.py using iris.csv
 
-print(ggplot(principal_label_df) +
-      aes(x="PC 1", y="PC 2", color="factor(label)") +
-      geom_point())
+df_multi_col = pd.read_csv('labelled.csv')
+
+# a csv file with one column + a label column
+
+df_1_col = pd.read_csv('labelled_col_1.csv')
+
+# a csv file with two columns + a label column
+df_2_col = pd.read_csv('labelled_col_2.csv')
+
+print(visualise(df_1_col))
+print(visualise(df_2_col))
+print(visualise(df_multi_col))
+
