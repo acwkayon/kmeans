02d93fcc4cd9552d8514b17258ca456d62ad236c
Anita Williams <acwkayon@gmail.com>
2022-02-18 19:23:15 -0800

    create a label variable, add error message if label column can’t be found, add option for output file save to be specified png or pdf, lint

--- visualise.py
+++ visualise.py
@@ -1,6 +1,6 @@
 # MLHub demonstrator and toolkit for kmeans.
 #
-# Time-stamp: <Sat 2022-02-12 13:13:41 -0800 Anita Williams>
+# Time-stamp: <Fri 2022-02-18 19:20:00 -0800 Anita Williams>
 #
 # Authors: Anita@togaware.com
 # License: General Public License v3 GPLv3
@@ -28,71 +28,82 @@ os.chdir(get_cmd_cwd())
                 default=sys.stdin)
 @click.option("-l", "--label",
               default="label",
-              help="identify the header for the column that contains the label")
+              help="identify the label column")
 @click.option("-o", "--output",
-              default= "output not given",
-              help="Filename of the PNG file to save the plot. If none specificed, plot will print on screen.")
-
-
+              default="output not given",
+              help="png/pdf to save output, will print if none given")
 def cli(csvfile, label, output):
-
-      
-      # Quit if there is no data to read
-      try:
+    # Quit if there is no data to read
+  
+    try:
         df = pd.read_csv(csvfile)
-      except pd.errors.EmptyDataError:
+    except pd.errors.EmptyDataError:
         click.echo("No data available.")
         sys.exit(1)
+       
+    # Return error and quit if label column is not able to be found
+   
+    if label not in df.columns:
+        click.echo("Column with label not identified")
+        sys.exit(1)
+         
+    # Line of dots if data is one dimension
+    
+    if len(df.columns) == 2:
+        # Find the non-label column
+       
+        header_1_col = list(df.columns.values)
+        header_1_col.remove(label)
+        p = (ggplot(df) +
+             aes(x=header_1_col[0], y=label, color=f"factor({label})") +
+             labs(color="Cluster") +
+             geom_point())
+     
+    # Scatter plot if data is two dimensions
+   
+    elif len(df.columns) == 3:
+          
+        # Find the non-label columns
+       
+        header_2_col = list(df.columns.values)
+        header_2_col.remove(label)
+        p = (ggplot(df) +
+             aes(x=header_2_col[0],
+                 y=header_2_col[1],
+                 color=f"factor({label})") +
+             labs(color="Cluster") +
+             geom_point())
+     
+    # Perform PCA if there are 2+ dimensions and visualise the 2 most
+    # significant components
+   
+    else:
+        pca = PCA(n_components=2)
+           
+        # Remove the label column for PCA
+      
+        df_for_pca = df.drop([label], axis=1)
+        principal_components = pca.fit_transform(df_for_pca)
+        principal_components
+        principal_df = pd.DataFrame(data=principal_components,
+                                    columns=['PC 1', 'PC 2'])
+        principal_label_df = pd.concat([principal_df,
+                                        df[[label]]],
+                                       axis=1)
+        p = (ggplot(principal_label_df) +
+             aes(x="PC 1", y="PC 2", color=f"factor({label})") +
+             labs(color="Cluster") +
+             geom_point())
 
-      df.rename(columns={label: "label"}, inplace = True)
-      
-      # Line of dots if data is one dimension
-      
-      if len(df.columns) == 2:
-            # Find the non-label column
-            header_1_col = list(df.columns.values)
-            header_1_col.remove('label')
-            vis_plot = (ggplot(df) +
-            aes(x=header_1_col[0], y='label', color="factor(label)")
-            + labs(color="Cluster") + geom_point())
-
-            
-      
-      # Scatter plot if data is two dimensions
-      elif len(df.columns) == 3:
-            
-            # Find the non-label columns
-            header_2_col = list(df.columns.values)
-            header_2_col.remove('label')
-            vis_plot = (ggplot(df) +
-            aes(x=header_2_col[0], y=header_2_col[1], color="factor(label)") +
-            labs(color="Cluster") + geom_point())
-      
-      # Perform PCA if there are 2+ dimensions and visualise the 2 most
-      # significant components 
-      # to do: add some context about PCA for end user
-      else:
-            pca = PCA(n_components=2)
-            
-            # Remove the label column for PCA
-            df_for_pca = df.drop(['label'], axis=1)
-            principal_components = pca.fit_transform(df_for_pca)
-            principal_components
-            principal_df = pd.DataFrame(data=principal_components
-                            , columns=['PC 1', 'PC 2']
-                            )
-            principal_label_df = pd.concat([principal_df,
-                                           df[['label']]],
-                                           axis=1)
-            vis_plot = (ggplot(principal_label_df) +
-            aes(x="PC 1", y="PC 2", color="factor(label)") 
-            + labs(color="Cluster")+ geom_point())
-      
-      
-      if output == "output not given":
-            print(vis_plot)
-      else: 
-            ggsave(plot = vis_plot, filename = output + ".png")
+    if output == "output not given":
+        print(p)
+    else:
+        if output[-3] == '.png':
+            ggsave(plot=p, filename=output)
+        if output[-3] == '.pdf':
+            ggsave(plot=p, filename=output)
+        else:
+            ggsave(plot=p, filename=output + '.png')
 
 if __name__ == "__main__":
     cli(prog_name="visualise")
